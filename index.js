963f1796b682dea6483d2d9d9ffecbd054dff7f3
nonyy <tomasz.hamerla@openmailbox.org>
2017-07-02 14:57:17 +0200

    Change dev msg

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Is it dev?!!");
+    response.end("Is it dev v2 ?!!");
 
 });
 
